e1af96e6dd1f28ef8edf0b0221310d4f561f0801
Eduardo Lopez <eduardo.lopez@elaniin.com>
2020-03-03 00:26:46 -0600

    Init files
    Init files

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    set_height();
+    /*$('#nav-icon').click(function() {
+         $(this).toggleClass('open');
+     });*/
+});
+
+$(window).resize(function() {
+    //aqui el codigo que se ejecutara cuando se redimencione la ventana
+    set_height();
+})
+
+function set_height() {
+    //height = $(window).height();
+    //$(".block").height(height);
+}
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
